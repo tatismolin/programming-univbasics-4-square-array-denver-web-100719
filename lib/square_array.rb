32e88040aa622e857420e62d73c5e33cd97faabb
Tatiana Smolin <tatismolin@gmail.com>
2019-09-17 04:33:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   # your code here
-  while array.length do
-    array.each {|i| = |i| **}
+  i = 0
+  while i < array.length do |number|
+    puts array[i]
+    i = i + number**2
   end
 end
