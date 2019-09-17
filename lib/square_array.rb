5e00bcc80eb5371ed640fc6a94accc06b2aa0d10
Tatiana Smolin <tatismolin@gmail.com>
2019-09-17 04:49:26 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = []# your code here
+  i = 0
+  while i < array.length do
+    new_array << array[i]**2
+    i += 1
+  end
+  new_array
 end
